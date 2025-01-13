030aad50823c5e2081478cf3974c45d1afa168d0
Nakul Saini <kanhasaini2017@gmail.com>
2025-01-13 19:02:54 +0530

    implemented REPL

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Uncomment this block to pass the first stage
-        // System.out.print("$ ");
-        System.out.print("$ ");
 
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        
-        System.out.println(input + ": command not found");
+        while(true) {
+            System.out.print("$ ");
+    
+            Scanner sc = new Scanner(System.in);
+            String input = sc.nextLine();
+            
+            System.out.println(input + ": command not found");
+        }
     }
 }
