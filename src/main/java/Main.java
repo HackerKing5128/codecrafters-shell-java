d26dee0d75145d38fdb75e780b1106061ce414ff
Nakul Saini <kanhasaini2017@gmail.com>
2025-01-13 20:31:56 +0530

    implemented type command - executable files v2.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,32 @@
+import java.nio.file.*;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+
+    public static String type_path(String input, String[] commands) {
+        // type builtin - executable files v2.0
+        
+        String path = System.getenv("PATH");    // get environment variable PATH
+        String[] directories = path.split(":"); // get all directories in PATH
+
+        String cmd = input.substring(5);    // Extract the command after "type "
+
+        //Search for the command in the PATH directories
+        for (String dir : directories) {
+            Path cmdPath = Paths.get(dir, cmd);
+            if (Files.exists(cmdPath) && Files.isExecutable(cmdPath)) {
+                return cmd + " is " + cmdPath.toString(); // Return the full path as a string
+            } /* else if (Arrays.asList(commands).contains(cmd)) {
+                return cmd + " is a shell builtin";   // functionality from type builtin v1.0
+            } */
+        }
+
+        return cmd + ": not found";
+
+        
+    }
+
     public static void main(String[] args) throws Exception {
 
         while(true) {
@@ -16,7 +41,6 @@ public class Main {
                 // exit builtin
 
                 int exitCode = Integer.parseInt(input.substring(5).trim()); // Extract from index 5, trim spaces, and parse
-
                 System.exit(exitCode);
 
             } else if(input.startsWith("echo")) {
@@ -28,13 +52,15 @@ public class Main {
             } else if(input.startsWith("type")) {
                 // type builtin
 
-                String cmd = input.substring(5).trim();
+                /*String cmd = input.substring(5).trim();
 
                 if(Arrays.asList(commands).contains(cmd)) {
                     System.out.println(cmd + " is a shell builtin");
                 } else {
                 System.out.println(cmd + ": not found");
-                }
+                } */
+
+                System.out.println(type_path(input, commands));
 
             } else {
                 // invalid command
@@ -42,4 +68,5 @@ public class Main {
             }
         }
     }
+
 }
