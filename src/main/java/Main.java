795bb54a4edae275282319bfc4e1e0a315b0c8c0
Nakul Saini <kanhasaini2017@gmail.com>
2025-01-13 19:09:55 +0530

    implemented exit command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,14 @@ public class Main {
             Scanner sc = new Scanner(System.in);
             String input = sc.nextLine();
             
-            System.out.println(input + ": command not found");
+            if (input.startsWith("exit")) {
+                int exitCode = Integer.parseInt(input.substring(5).trim()); // Extract from index 5, trim spaces, and parse
+
+                System.exit(exitCode);
+
+            } else {
+                System.out.println(input + ": command not found");
+            }
         }
     }
 }
